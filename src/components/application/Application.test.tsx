249166d5dbc8b12a89f83d84b90c0ffea18a2535
saurabh4530 <kakadesaurabh453@gmail.com>
2024-10-20 09:27:26 +0530

    1.3

--- src/components/application/Application.test.tsx
+++ src/components/application/Application.test.tsx
@@ -7,9 +7,23 @@ import Application from "./Application";
 describe("application",()=>{
     test("renders correctly",()=>{
         render(<Application/>)
-        const textelement=screen.getByRole("textbox")
-        expect(textelement).toBeInTheDocument()
+        const textelement=screen.getByRole("textbox");
+        expect(textelement).toBeInTheDocument();
+
+        const job_location=screen.getByRole("combobox");
+        expect(job_location).toBeInTheDocument();
+
+        const termsElement=screen.getByRole("checkbox");
+        expect(termsElement).toBeInTheDocument();
+
+
+        const submitBtnElement=screen.getByRole("button");
+        expect(submitBtnElement).toBeInTheDocument();
+
+
+
     })
+  
 })
 
 
