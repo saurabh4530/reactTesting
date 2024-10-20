6c2c81f90f061660c4ff0c90e64cc185914f7282
saurabh4530 <kakadesaurabh453@gmail.com>
2024-10-20 10:02:59 +0530

    1.4

--- src/components/application/Application.tsx
+++ src/components/application/Application.tsx
@@ -1,39 +1,35 @@
-import React from 'react'
+import React from "react";
 
 function Application() {
-    return (
-        <form >
-            <div>
-                <label htmlFor='name'>Name</label>
-                <input type="text" id='name' />
-            </div>
-            <div>
-                <label htmlFor="job_location">Job location</label>
-                <select id="job_location">
-                    <option value="">select a country</option>
-                    <option value="us">united state</option>
-                    <option value="uk">united kingdom</option>
-                    <option value="in">india</option>
+  return (
+    <form>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" />
+      </div>
+      <div>
+        <label htmlFor="bio">Bio</label>
+        <textarea id="bio"  name="bio"/>
+      </div>
+      <div>
+        <label htmlFor="job_location">Job location</label>
+        <select id="job_location">
+          <option value="">select a country</option>
+          <option value="us">united state</option>
+          <option value="uk">united kingdom</option>
+          <option value="in">india</option>
+        </select>
+      </div>
+      <div>
+        <label>
+          <input type="checkbox" id="terms" />I agree to the terms and
+          conditions
+        </label>
+      </div>
 
-
-
-
-
-                </select>
-            </div>
-            <div>
-
-                <label >
-                    <input type="checkbox" id="terms" />I agree to the terms and conditions
-
-                </label>
-            </div>
-
-            <button >Submit</button>
-
-
-        </form>
-    )
+      <button>Submit</button>
+    </form>
+  );
 }
 
-export default Application
+export default Application;
